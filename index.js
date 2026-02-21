505781ae6e7af8a176b39998ba88eeb4f45fa08b
MAlmamunislam <almamun2026islam@gmail.com>
2026-02-22 05:02:20 +0600

    UI COMPETTE

---
+++ index.js
@@ -0,0 +1,5 @@
+ console.log(document.getElementsByClassName('apple-button'));
+ console.log(document.getElementsByClassName('reject-button'));
+ console.log(document.getElementsByClassName('delete-btn'));
+ console.log(document.getElementsByClassName('Not-Applied'));
+ 
